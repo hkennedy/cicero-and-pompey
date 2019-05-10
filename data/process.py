c50a576ee34ffa6c377a7fea6f3b97ede94f6cca
hkennedy <hek@commonwealthcrypto.com>
2019-05-10 15:59:41 -0400

    Add comments

--- data/process.py
+++ data/process.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python
 import math
 
+#Example line of data:
 #49,1,17,Att. 7.10,Atticus,Cicero,Mention,-1
+
+#This function makes sure every day or month is two characters.
+#E.g. '02' instead of '2'.
 def c(m):
     return m if len(m) == 2 else '0' + m
 
+#Create a dictionary mapping the year to an empty list for storing
+#the cleaned data points
 years = {str(k): list() for k in range(1947, 1956)}
 
+#Open the csv fild and read in every line
 with open('raw_data.csv', 'r') as f:
     lines = f.readlines()[1:]
     for l in lines:
@@ -18,13 +25,19 @@ with open('raw_data.csv', 'r') as f:
         to = s[4]
         fro = s[5]
         sentiment = float(s[7]) if s[7] else None
-        
+
+        #Make sure all of the necessary fields are present. If they are, add the data point
+        #to the year's corresponding list.
         if year != None and month != None and sentiment != None:
             years[year].append(('Positive' if sentiment > 0 else 'Negative' if sentiment < 0 else 'Neutral', year + '-' + month + '-' + day, letter + ' (To: ' + to + '; From: ' + fro + ')'))
 
+#Sort each list so that the graphs will show up Positive,
+#then Neutral, then Negative
 a = {'Positive': 0, 'Neutral': 1, 'Negative': 2}
 for k in years:
     years[k] = sorted(years[k], key=lambda k: a[k[0]])
+
+#Open and read the beginning and ending file templates
 beg = ""
 end = ""
 with open('beg.html', 'r') as b:
@@ -32,9 +45,12 @@ with open('beg.html', 'r') as b:
 with open('end.html', 'r') as e:
     end = e.read()
 
+#This function takes a datapoint and writes the javascript line that
+#adds it to the graph
 def createLine(p):
     return "[ '" + p[0] + "', null, '" + p[2] + "', new Date('" + p[1] + "'), new Date((new Date('" + p[1] + "')).getTime() + 1400*60000) ]"
 
+#For each list of datapoints, create an HTML page with all of the data inserted
 for k in years:
     if len(years[k]) == 0:
         continue
